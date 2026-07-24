fc46f6a708247f22e06cac2e69cda0c86dc354e5
Azzam Saleh <eng.azzamsaleh@gmail.com>
2026-07-24 14:13:40 +0300

    Connect frontend to Railway backend

--- tasks-manager-frontend/src/environments/environment.ts
+++ tasks-manager-frontend/src/environments/environment.ts
@@ -5,9 +5,6 @@
 export const environment = {
   production: true,
 
-  apiUrl:
-    'https://tasks-manager-backend-3kvz.onrender.com/api',
-
-  wsUrl:
-    'wss://tasks-manager-backend-3kvz.onrender.com/ws'
+   apiUrl: 'https://tasks-manager-backend-production-5966.up.railway.app/api',
+  wsUrl: 'wss://tasks-manager-backend-production-5966.up.railway.app/ws'
 };
